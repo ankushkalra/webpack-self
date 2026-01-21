9fcab48747e677bf04effead22d58df20f44c628
Ankush Kalra <ankushkalra2@gmail.com>
2026-01-21 19:03:39 +0530

    added script-src-prefix option

--- bin/webpack.js
+++ bin/webpack.js
@@ -1,13 +1,46 @@
 var webpack = require("../lib/webpack");
+var fs = require("fs");
+var path = require("path");
 
 var argv = require("optimist")
-.usage("Usage: $0 <input> <output>")
-.demand(1)
-  .argv;
+  .usage("Usage: $0 <entry> <output>")
+
+  .boolean("single")
+  .describe("single", "Disable code splitting")
+  .default("single", false)
+
+  .string("script-src-prefix")
+  .describe("single", "Path prefix for javascript loading")
+
+  .demand(1).argv;
+
+var output = argv._[1];
+
+if (output && output[0] !== "/" && output[1] !== ":") {
+  output = path.join(process.cwd(), output);
+}
 
 var options = {
   entry: argv._[0],
-  output: argv._[1] || "output.js",
 };
 
-webpack(options);
+if (argv["script-src-prefix"]) {
+  options.scriptSrcPrefix = argv["script-src-prefix"];
+}
+
+output = output || path.join(process.cwd(), "js", "web.js");
+if (!options.outputDirectory) options.outputDirectory = path.dirname(output);
+var basename = path.basename(output);
+if (!options.output) options.output = basename;
+
+if (!options.outputPostfix) options.outputPostfix = "." + path.dirname(output);
+var outExists = fs.existsSync(options.outputDirectory);
+if (!outExists) fs.mkdirSync(options.outputDirectory);
+
+webpack(options, function (err, stats) {
+  if (err) {
+    console.error(err);
+    return;
+  }
+  console.log("stats = ", stats);
+});
