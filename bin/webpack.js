9c424adb7c2ec2fe004a77f0f069d8b9468cb2cc
Ankush Kalra <ankushkalra2@gmail.com>
2026-01-22 12:20:02 +0530

    added more options

--- bin/webpack.js
+++ bin/webpack.js
@@ -9,8 +9,22 @@ var argv = require("optimist")
   .describe("single", "Disable code splitting")
   .default("single", false)
 
+  .boolean("min")
+  .describe("min", "minifiy output")
+  .default("min", false)
+
+  .boolean("filenames")
+  .describe("filenames", "Output Filenames Info file")
+  .default("filenames", false)
+
   .string("script-src-prefix")
   .describe("single", "Path prefix for javascript loading")
+
+  .string("options")
+  .describe("options", "Options JSON file")
+
+  .string("libary")
+  .describe("libary", "Stores the exports in this variable")
 
   .demand(1).argv;
 
@@ -20,12 +34,28 @@ if (output && output[0] !== "/" && output[1] !== ":") {
   output = path.join(process.cwd(), output);
 }
 
-var options = {
-  entry: argv._[0],
-};
+var options = {};
+
+if (argv.options) {
+  options = JSON.parse(fs.readFileSync(argv.options, "utf-8"));
+}
+
+options.entry = argv._[0];
 
 if (argv["script-src-prefix"]) {
   options.scriptSrcPrefix = argv["script-src-prefix"];
+}
+
+if (argv.min) {
+  options.minimize = true;
+}
+
+if (argv.libary) {
+  options.libary = argv.libary;
+}
+
+if (argv.filenames) {
+  options.includeFilenames = true;
 }
 
 output = output || path.join(process.cwd(), "js", "web.js");
